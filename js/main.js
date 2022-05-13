1ec0dc3d60dda8113d864a02df529ee142c41244
Seralyn Campbell <seralyncampbell@gmail.com>
2022-05-13 19:05:09 +0900

    finished adding all content/markup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+// Accordion FAQ Logic
+// Add the accordion-bottomBorder class to all opened accordions
+
+// faqQuestion1 = document.getElementById("#faqQuestion1");
+// faqAnswer1 = document.getElementById("#faqAnswer1");
+faqQuestion2 = document.getElementById("#faqQuestion2").addEventListener("click", (e) => {....});
+faqAnswer2 = document.getElementById("#faqAnswer2");
+// faqQuestion3 = document.getElementById("#faqQuestion3");
+// faqAnswer3 = document.getElementById("#faqAnswer3");
+// faqQuestion4 = document.getElementById("#faqQuestion4");
+// faqAnswer4 = document.getElementById("#faqAnswer4");
+// faqQuestion5 = document.getElementById("#faqQuestion5");
+// faqAnswer5 = document.getElementById("#faqAnswer5");
+// faqQuestion6 = document.getElementById("#faqQuestion6");
+// faqAnswer6 = document.getElementById("#faqAnswer6");
+// faqQuestion7 = document.getElementById("#faqQuestion7");
+// faqAnswer7 = document.getElementById("#faqAnswer7");
+
+const toggleFAQAnswer = (e) => {
+  if (answerID.className.indexOf("block") == -1) {
+    answerID.className + "block";
+    answerID.previousElementSibling.className + "border-b-2";
+  } else {
+    answerID.className = answerID.className.replace("block", "hidden");
+    answerID.previousElementSibling.className =
+      answerID.previousElementSibling.className.replace("border-b-2", "");
+  }
+};
+document
+  .querySelectorAll(".faqBox")
+  .addEventListener("click", function(event){...});
+
 // ----------- SCROLL ANIMATION LOGIC -------------
 gsap.registerPlugin(ScrollTrigger);
 
@@ -56,21 +88,7 @@ function init() {
     },
   });
 
-  //   gsap.from(["#specCol1", "#specCol2", "specCol3"], {
-  //     autoAlpha: 0,
-  //     xPercent: -10,
-  //     duration: 0.4,
-  //     stagger: 0.2,
-
-  //     scrollTrigger: {
-  //       trigger: "#fasterThan",
-  //       start: "top bottom", //top of trigger element (#intro in this case) is at the top of the viewport
-  //       end: "top top-=300",
-  //       scrub: true,
-  //     },
-  //   });
-
-  // PIN WEBSITE THUMBS AND THEN ENLARGE AND FADE THEM
+  // Reveal specs one after another
   ScrollTrigger.batch(["#specCol1", "#specCol2", "#specCol3"], {
     // interval: 0.1, // time window (in seconds) for batching to occur.
     // batchMax: 3,   // maximum batch size (targets)
@@ -85,8 +103,66 @@ function init() {
         // stagger: 0.1,
         pin: true,
       }),
-    // also onLeave, onEnterBack, and onLeaveBack
-    // also most normal ScrollTrigger values like start, end, etc.
+  });
+
+  // Reveal dashed boxes one after another
+  ScrollTrigger.batch(["#dashedBox1", "#dashedBox2", "#dashedBox3"], {
+    // interval: 0.1, // time window (in seconds) for batching to occur.
+    // batchMax: 3,   // maximum batch size (targets)
+    onEnter: (batch) =>
+      gsap.from(batch, {
+        autoAlpha: 0,
+        xPercent: -10,
+        // scale: 3,
+        duration: 0.3,
+        delay: 0.3,
+        // ease: "power1.in",
+        stagger: 0.1,
+        pin: true,
+      }),
+  });
+
+  // Reveal popup menus over hello world window one after another
+
+  gsap.from(["#popUp1"], {
+    autoAlpha: 0,
+    xPercent: 10,
+    duration: 0.2,
+
+    scrollTrigger: {
+      trigger: "#browserPreview",
+      start: "top bottom", //top of trigger element (#intro in this case) is at the top of the viewport
+      end: "top top",
+      scrub: true,
+    },
+  });
+
+  gsap.from("#popUp2", {
+    autoAlpha: 0,
+    xPercent: -10,
+
+    scrollTrigger: {
+      trigger: "#browserPreview",
+      start: "top bottom", //top of trigger element (#intro in this case) is at the top of the viewport
+      end: "top top",
+      scrub: true,
+    },
+  });
+
+  ScrollTrigger.batch(["#windowFromRight", "#windowFromLeft", ".featureBox"], {
+    // interval: 0.1, // time window (in seconds) for batching to occur.
+    // batchMax: 3,   // maximum batch size (targets)
+    onEnter: (batch) =>
+      gsap.from(batch, {
+        autoAlpha: 0,
+        xPercent: -5,
+        // scale: 3,
+        duration: 0.3,
+        delay: 0.3,
+        // ease: "power1.in",
+        stagger: 0.1,
+        pin: true,
+      }),
   });
 }
 
